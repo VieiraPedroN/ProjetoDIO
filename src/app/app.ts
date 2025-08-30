c1c2286dd0b22bc83b7e117eec352566e5d4424c
Pedro Vieira <pedronicolas576@gmail.com>
2025-08-30 16:14:26 -0300

    Incluindo arquitetura principal de arquivos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { MenuBar } from './components/menu-bar/menu-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, MenuBar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('store');
